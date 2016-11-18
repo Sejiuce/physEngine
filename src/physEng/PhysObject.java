8efa7ffd32bfe6b3736e80bf76cf06080dc6a303
Sam Wade <u6053755@anu.edu.au>
2016-11-18 11:17:12 +1100

    It has begun

---
+++ src/physEng/PhysObject.java
@@ -0,0 +1,14 @@
+package physEng;
+
+/**
+ * Created by Promethean on 18/11/2016.
+ */
+public class PhysObject {
+    double xVelocity; //velocity in x direction
+    double yVeloctiy; //velocity in y direction
+    double xAcceleration; //acceleration in x direction
+    double yAcceleration; //acceleration in y direction
+    double mass; //mass of object
+    double xCOM; //centre of mass in x direction
+    double yCOM; //centre of mass in y direction
+}
